66d861abeffaa99373a488c5d1991698a1b704c5
Dmitry Kutsenko <d2emonium@gmail.com>
2018-09-04 21:55:07 +0300

    Go direction

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -3,16 +3,35 @@ import express from 'express';
 import Room from '../models/room';
 const router = express.Router();
 
-/* GET users listing. */
+const go = (room, direction) => {
+    if (!room) throw new Error('No room found.');
+    if (!direction) throw new Error('That\'s not a valid direction.');
+
+    let newRoomId = room.exits[direction];
+    // if (newRoomId <= 0) newRoomId = door(-newRoomId);
+    if (newRoomId <= 0) throw new Error("You can't go that way");
+
+    return Room.findOne({roomId: newRoomId});
+};
+
 router.get('/', (req, res) => {
     Room.find().select({"roomId": 1})
         .then(response => res.json({rooms: response}))
         .catch(error => res.status(500).json({error: error}));
 });
 
+router.get('/:room_id/:direction', (req, res) => {
+    const room_id = req.params.room_id;
+    const direction = req.param.direction;
+
+    Room.findOne({roomId: roomId})
+        .then(room => go(room, direction))
+        .then(response => res.json({room: response}))
+        .catch(error => res.status(500).json({error: error}));
+});
+
 router.get('/:roomId', (req, res) => {
     const roomId = req.params.roomId;
-    console.log(roomId);
     Room.findOne({roomId: roomId})
         .then(response => res.json({room: response}))
         .catch(error => res.status(500).json({error: error}));
@@ -34,6 +53,8 @@ router.put('/:room_id', (req, res) => {
         })
         .then(response => res.json({room: response, result: result}))
         .catch(error => res.status(500).json({error: error}));
+
+
 });
 
 router.delete('/:room_id', (req, res) => {
